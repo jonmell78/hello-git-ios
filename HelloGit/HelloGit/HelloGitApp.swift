bb0343e7328323e4b84ba59efcb2999b0df9d2f8
M1 <M1@jonmell.co.uk>
2026-02-10 15:44:08 +0000

    Initial iOS project setup

---
+++ HelloGit/HelloGit/HelloGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloGitApp.swift
+//  HelloGit
+//
+//  Created by Jon Mell on 10/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct HelloGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HelloGit/HelloGitTests/HelloGitTests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloGitTests.swift
+//  HelloGitTests
+//
+//  Created by Jon Mell on 10/02/2026.
+//
+
+import Testing
+@testable import HelloGit
+
+struct HelloGitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
